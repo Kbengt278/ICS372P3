3329cfc035e1054345b75a787aa2735f4be8e646
tylerThrash <tjthrash2112@outlook.com>
2017-03-20 01:30:45 -0500

    Fix for check in.

--- Items/Item.java
+++ Items/Item.java
@@ -80,13 +80,18 @@ public class Item implements Serializable {
 	@Override
 	public String toString()
 	{
-		return "Id = " + getId() +
+		String message = "Id = " + getId() +
     	       " " + getType() + " " +
-               " Name = " + getName() +
-               " Due Date = " +
-               (getDateDue().get(Calendar.MONTH) + 1) +
-               "/" + getDateDue().get(Calendar.DAY_OF_MONTH)
-                + "/" + getDateDue().get(Calendar.YEAR) + "\n";
+               " Name = " + getName();
+		if(dateDue != null)
+		{
+			message += " Due Date = " +
+               (dateDue.get(Calendar.MONTH) + 1) +
+               "/" + dateDue.get(Calendar.DAY_OF_MONTH)
+                + "/" + dateDue.get(Calendar.YEAR) + "\n";
+		}
+		
+		return message;
 	}
     
 
