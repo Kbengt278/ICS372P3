be2b398467005f5b7f8b4c454683cea720f57d2a
mattMilton <matthewmilton@msn.com>
2017-04-10 22:30:57 -0500

    all files from source directory
    create an android studio project with empty source directory and import these; I am not yet done with the check in / check out activity. I had planned on finishing it before committing, but realize I might not get to it again until Wednesday.

---
+++ src/main/java/com/cheetahs/libraryapp/CheckInOutActivity.java
@@ -0,0 +1,16 @@
+package com.cheetahs.libraryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RadioButton;
+
+public class CheckInOutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_in_out);
+
+
+    }
+}
---
+++ src/main/java/com/cheetahs/libraryapp/DisplayLibraryActivity.java
@@ -0,0 +1,13 @@
+package com.cheetahs.libraryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayLibraryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_library);
+    }
+}
